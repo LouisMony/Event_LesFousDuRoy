1cb6de15765429fe4480c7f4a909bfb01431e643
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2023-03-15 21:34:32 +0100

    Update Base Acces

--- src/assets/js/http-common.js
+++ src/assets/js/http-common.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 export const http = axios.create({
   baseURL: `https://api.airtable.com/v0/appIikQa2F0vLZo8R/`,
-  headers: {'Authorization': 'Bearer key1knTuZ7MwzCLsY'},
+  headers: {'Authorization': 'Bearer patKLE00iydkq4kWc.76cdef0c30ea1b7c43b48fb33413db2f85ef331643d19979bab209426e14fdb2'},
 })
+
+//TOKEN D'ACCES
+//patKLE00iydkq4kWc.76cdef0c30ea1b7c43b48fb33413db2f85ef331643d19979bab209426e14fdb2
+//key1knTuZ7MwzCLsY
 
